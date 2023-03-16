a0205dcda30c2f5851c1a3acc7422e0834c16e93
DJALMA BRUNO SILVEIRA LUIZ <80864812+djalmabsilveira@users.noreply.github.com>
2023-03-16 08:27:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,29 +6,20 @@ const middlewares = jsonServer.defaults();
 
 const port = process.env.port || 3001;
 
+const cors = require('cors');
+
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
   console.log("js");
 });
 
-module.exports = {
-  trailingSlash: false,
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
-        ],
-      },
-    ];
-  },
-  async redirects() {
-    return [];
-  }
-};
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    })
+);
+server.options('*', cors());
